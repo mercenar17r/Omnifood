ec585a7e50637aabeb5d9c17332c146e86be9d5e
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-11 22:59:10 +0200

    toggle mobile nav

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
