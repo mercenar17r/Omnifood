f552afcb0cd3be115ccb6350d66de27c025b56e3
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-12 22:56:06 +0200

    add event listener for href

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+  });
+});
