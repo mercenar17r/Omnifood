9bee2514aeb983d400c72ae3cb8837b3bffbc603
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-12 23:01:32 +0200

    add  scroll behavior

--- js/index.js
+++ js/index.js
@@ -6,12 +6,18 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-
+// Smooth scrolling animation
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
   });
 });
