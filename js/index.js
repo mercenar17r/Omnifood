ecc81f9594bcfd2f559e5a46a73bf4d44024954d
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-14 00:01:31 +0200

    remove class when moving up

--- js/index.js
+++ js/index.js
@@ -42,11 +42,15 @@ const obs = new IntersectionObserver(
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
     }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
     // In the viewport
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
